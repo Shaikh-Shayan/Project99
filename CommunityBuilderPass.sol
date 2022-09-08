b401dcc79aa66e54d8cc89a0903e94680969e746
Divya Nautiyal <109058690+DivyaNautiyal-Rev@users.noreply.github.com>
2022-09-08 11:17:36 +0530

    Update CommunityBuilderPass.sol

--- CommunityBuilderPass.sol
+++ CommunityBuilderPass.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts@4.7.3/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts@4.7.3/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts@4.7.3/access/AccessControl.sol";
 
-contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable, EIP712{
+contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable, EIP712, AccessControl{
     event Attest(address indexed to, uint256 indexed tokenId);
     event Revoke(address indexed to, uint256 indexed tokenId);
+    event Reedemed(address indexed buyer, uint256 indexed tokenId);
 
+    //NFT Voucher contains all the information that will go in the actual NFT
     struct NFTVoucher {
         uint256 tokenId;
         uint256 minPrice;
         string uri;
         address buyer;
+        //The signature proves that the NFT creator authorized the creation of the specific NFT described in the voucher.
         bytes signature;
     }
 
@@ -23,64 +27,66 @@ contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable, EIP712{
     string private constant SIGNING_VERSION = "1";
     uint256 immutable MAX_SUPPLY;
     uint256 minted = 0;
-    mapping(address => mapping(uint256 => bool)) redeemed;
-    address private minter;
+    
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    //signature tracker
+    mapping(bytes => bool) signatureUsed;
 
     constructor(address signer, uint256 maxSupply) ERC1155("CommunityBuilderPass") EIP712(SIGNING_DOMAIN, SIGNING_VERSION){
-        minter = signer;
+        _setupRole(MINTER_ROLE, signer);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         MAX_SUPPLY = maxSupply;
     }
 
+    //sets the base URI
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
+    //takes a voucher as an argument and let's the user redeem the signed voucher
     function redeem(NFTVoucher calldata voucher)
         public
         payable
     {   
-        require(minter == _verify(voucher), "Invalid signer");
-        require(redeemed[voucher.buyer][voucher.tokenId] != true, "Already redeemed!");
+        //Check if the signature is valid and belongs to the account that's authorized to mint NFTs
+        address signer = _verify(voucher);
+        require(hasRole(MINTER_ROLE, signer), "Invalid signer");
+
+        //Check to see if user has already used the signature
+        require(!signatureUsed[voucher.signature], "Signature has already been used.");
+        
+        //This contract represents only 1 single NFT hence only tokenId 1 is possible
         require(voucher.tokenId == 1 , "Token doesn't exist");
+        
         require(minted + 1 <= MAX_SUPPLY, "Not enough supply");
         require(msg.value >= voucher.minPrice, "Not enough ethers sent");
 
         _mint(voucher.buyer, voucher.tokenId, 1, "");
 
         minted += 1;
-        redeemed[voucher.buyer][voucher.tokenId] = true;
+        signatureUsed[voucher.signature] = true;
+
+        emit Reedemed(voucher.buyer, voucher.tokenId);
     }
 
-    //function mint(address account, uint256 id)
-    //  public
-    //   payable
-    //{   
-    //    require(id==1, "Token doesn't exists");
-    //    require(minted+1 <= MAX_SUPPLY, "Not enough supply");
-    //    require(msg.value >= 1 ether, "Not enough amount sent to buy NFT");
-    //    _mint(account, id, 1, "");
-    //    minted += 1;
-    //}
-    
+
+    //burns the nft
     function burn(uint256 tokenId)
         external 
     {
         require(balanceOf(msg.sender, tokenId) > 1, "You do not own any NFT!");
         _burn(msg.sender, tokenId, 1);
     }
-
-    function revoke(address tokenOwner, uint256 tokenId)
-        external 
-        onlyOwner
-    {
-        _burn(tokenOwner, tokenId, 1);
-    }
-
+    
+    //let's the owner withdraw the funds from the contract
     function withdraw() public onlyOwner{
         require(address(this).balance >0, "Balance is 0");
         payable(owner()).transfer(address(this).balance);
     }
 
+
+    //makes NFT non-transferable 
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal 
         override 
@@ -89,6 +95,8 @@ contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable, EIP712{
         require(from == address(0) || to == address(0), "You can't transfer this NFT");
     }
 
+
+    //emits the event based on whether token is minted or burned.
     function _afterTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal 
         override 
@@ -99,14 +107,17 @@ contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable, EIP712{
         }else if(to == address(0)){
             emit Revoke(to, ids[0]);
         }
-
     }
+    
 
+    //verifies signature against input and recovers address, or reverts transaction if signature is invalid
     function _verify(NFTVoucher calldata voucher) internal view returns (address) {
         bytes32 digest = _hash(voucher);
         return ECDSA.recover(digest, voucher.signature);
     }
 
+
+    //returns the hash of the argument passed
     function _hash(NFTVoucher calldata voucher) internal view returns (bytes32) {
         return _hashTypedDataV4(keccak256(abi.encode(
             keccak256("NFTVoucher(uint256 tokenId,uint256 minPrice,string uri,address buyer)"),
@@ -118,5 +129,9 @@ contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable, EIP712{
     }
 
 
+    //override required by solidity
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 
 }
