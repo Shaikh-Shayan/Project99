943e801be1240bcbd9ba8253f5fd8c783a35f942
Divya Nautiyal <109058690+DivyaNautiyal-Rev@users.noreply.github.com>
2022-09-26 13:37:32 +0530

    Update ContentPass_SocialPass.sol

--- ContentPass_SocialPass.sol
+++ ContentPass_SocialPass.sol
@@ -5,62 +5,87 @@ import "@openzeppelin/contracts@4.7.3/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC1155/extensions/ERC1155Burnable.sol";
 
+//["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x23079599b4950D89429F1C08B2ed2DC820955Fd5"]
+
 contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable{
 
-    event Airdropped(address account, uint256 tokenId);
+    event Airdropped(address account, uint256 amount, uint256 tokenId);
     event NotAirdropped(address account, uint256 tokenId);
-    event Claimed(address account, uint256 tokenId);
+    event Claimed(address account, uint256 amount, uint256 tokenId);
     event NFTMinted(address account, uint256 tokenId);
     event NFTBurned(address account, uint256 tokenId);
     event RemovedFromAllowlist(address account, uint256 tokenId);
     event AddedToAllowlist(address account, uint256 tokenId);
     
-    mapping(uint256 => uint256) COPIES;
+    mapping(uint256 => uint256) MAX_COPIES;
     mapping(uint256 => uint256) minted;
     uint256 creationTime;
     uint256 burnTime;
 
-    mapping(uint256=>mapping(address => bool)) public claimed;
     mapping(uint256=>mapping(address => bool)) public allowlist;
-    mapping(uint256=>mapping(address => bool)) public airdropped;
+    mapping(uint256=>mapping(address => uint256)) public airdropped;
 
-    constructor(uint256 contentPassCopies, uint256 socialPassCopies,address[] memory _whitelistForSocialPass, address[] memory _whitelistForContentPass) ERC1155("CommunityBuilderPass"){
+    struct NFTAirdrop{
+        address receiver;
+        uint256 amount;
+    }
+
+    constructor(uint256 contentPassCopies, uint256 socialPassCopies,address[] memory _whitelistForContentPass, address[] memory _whitelistForSocialPass) ERC1155("CommunityBuilderPass"){
         creationTime = block.timestamp;
         //365(days)*24(hours)*60(minutes)*60(seconds) = 31536000 seconds
         burnTime = creationTime + 31536000;
-        COPIES[1] = contentPassCopies;
-        COPIES[2] = socialPassCopies;
-
-        for(uint i=0; i< _whitelistForSocialPass.length; i++){
-            allowlist[1][_whitelistForSocialPass[i]] = true;
-        }
+        MAX_COPIES[1] = contentPassCopies;
+        MAX_COPIES[2] = socialPassCopies;
 
         for(uint i=0; i< _whitelistForContentPass.length; i++){
-            allowlist[2][_whitelistForContentPass[i]] = true;
+            allowlist[1][_whitelistForContentPass[i]] = true;
+        }
+
+        for(uint i=0; i< _whitelistForSocialPass.length; i++){
+            allowlist[2][_whitelistForSocialPass[i]] = true;
         }
     }
 
-    //airdrops nfts to all the accounts passed as paramater to the function
-    function airdrop(uint256 tokenId, address[] memory accounts) 
+    //airdrops nfts to the account passed as param to the function
+    function airdrop(uint256 tokenId, address account, uint256 amount) 
         external
         onlyOwner 
     {
         require(tokenId == 1 || tokenId == 2, "TokenId doesn't exists!");
 
         //check if number of airdropped nft exceeds Maximum Supply
-        require(minted[tokenId]+accounts.length <= COPIES[tokenId], "Not enough supply");
+        require(minted[tokenId]+1 <= MAX_COPIES[tokenId], "Not enough supply");
 
-        for(uint i = 0; i < accounts.length; i++) {
-            //Skip the address if it's not present in the allowlist or if it has been airdropped nft already
-            if(allowlist[tokenId][accounts[i]] == false || airdropped[tokenId][accounts[i]] == true ){
-                emit NotAirdropped(accounts[i], tokenId);
-                continue;
+        require(allowlist[tokenId][account], "Address not present in the allowlist");
+
+        airdropped[tokenId][account] += amount;
+        emit Airdropped(account, amount, tokenId);
+        
+    }
+
+    //airdrops nfts to all the accounts passed as paramater to the function
+    function airdropToMultipleAccount(uint256 tokenId, NFTAirdrop[] memory toAirdrop) 
+        external
+        onlyOwner 
+    {
+        require(tokenId == 1 || tokenId == 2, "TokenId doesn't exist");
+
+        for(uint256 i =0; i < toAirdrop.length; i++){
+            address receiver = toAirdrop[i].receiver;
+            uint256 amount = toAirdrop[i].amount;
+            //check if number of airdropped nft exceeds Maximum Supply
+            require(minted[tokenId] + amount <= MAX_COPIES[tokenId], "Not enough supply");
+            
+            if(allowlist[tokenId][receiver]){
+                airdropped[tokenId][receiver] += amount;
+                emit Airdropped(receiver, amount, tokenId);
+            }else{
+                emit NotAirdropped(receiver, tokenId);
             }
-
-            airdropped[tokenId][accounts[i]] = true;
-            emit Airdropped(accounts[i], tokenId);
+        
         }
     }
+
 
     //adds the list of addresses to the Allowlist
     function addToAllowlist(address[] memory _recipients, uint256 tokenId) 
@@ -86,18 +111,20 @@ contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable{
     }
 
     //mints the nft to the address(msg.sender) who was aidropped the nft 
-    function claim(address account, uint256 tokenId) external {
+    function claim(address account, uint256 tokenId) 
+        external 
+    {
+        uint256 amount = airdropped[tokenId][account];
 
         require(tokenId == 1 || tokenId == 2, "TokenId doesn't exists!");
-        require(airdropped[tokenId][account] == true, "You don't have any NFT!");
-        require(claimed[tokenId][account] == false, "NFT Already Claimed!");
+        require(amount> 0, "You don't have any NFT!");
 
-        claimed[tokenId][account] = true;
+        airdropped[tokenId][account] = 0; 
 
-        _mint(account, tokenId, 1, "");
-        minted[tokenId] += 1;
+        _mint(account, tokenId, amount, "");
+        minted[tokenId] += amount;
         
-        emit Claimed(account, tokenId);
+        emit Claimed(account, amount, tokenId);
     }
 
     //update the number of copies
@@ -108,7 +135,7 @@ contract CommunityBuilderPass is ERC1155, Ownable, ERC1155Burnable{
         require(tokenId == 1|| tokenId == 2, "TokenId doesn't exists");
         require(newCopies>0, "Copies cannot be zero");
 
-        COPIES[tokenId] = newCopies;
+        MAX_COPIES[tokenId] = newCopies;
     }
 
     //sets the base URI
