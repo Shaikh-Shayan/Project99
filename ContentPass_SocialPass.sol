a15c3f53e97186ecd5e3b99934f9ae89b91255e4
Divya Nautiyal <109058690+DivyaNautiyal-Rev@users.noreply.github.com>
2022-09-27 10:58:19 +0530

    Update ContentPass_SocialPass.sol

--- ContentPass_SocialPass.sol
+++ ContentPass_SocialPass.sol
@@ -4,49 +4,58 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts@4.7.3/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts@4.7.3/utils/cryptography/draft-EIP712.sol";
-import "@openzeppelin/contracts@4.7.3/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts@4.7.3/security/ReentrancyGuard.sol";
 
-//["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x23079599b4950D89429F1C08B2ed2DC820955Fd5"]
-contract NFT is ERC1155, ERC1155Burnable, EIP712, Ownable, ReentrancyGuard {
-    /*
-    @dev The event 'NFTPurchased' must be emitted when an account purchases the NFT
-    The uint256-type member 'tokenId' takes Token ID
-    The uint256-type member 'nonce' takes nonce
-    The uint256-type member 'copies' takes copies
-    The address-type member 'account' takes buyer's address
-    The uint256-type member 'amount' takes token amount
-    */
-    event NFTPurchased(
-        uint256 tokenId,
-        uint256 nonce,
-        uint256 copies,
-        address buyer,
-        uint256 amount
-    );
+contract CommunityBuilderPass is
+    ERC1155,
+    Ownable,
+    ERC1155Burnable,
+    ReentrancyGuard
+{
     /*
     @dev The event 'Airdropped' must be emitted when an account is airdropped tokens
     The address-type member 'account' takes receiver's address
+    The uint256-type member 'amount' takes token amount
     The uint256-type member 'tokenId' takes Token ID
     */
-    event Airdropped(address account, uint256 tokenId);
+    event Airdropped(address account, uint256 amount, uint256 tokenId);
     /*
-    @dev The event 'MemberPassClaimed' must be emitted when a pass is claimed
+    @dev The event 'NotAirdropped' must be emitted when an airdrop fails
     The address-type member 'account' takes receiver's address
     The uint256-type member 'tokenId' takes Token ID
     */
-    event MemberPassClaimed(address account, uint256 tokenId);
+    event NotAirdropped(address account, uint256 tokenId);
+    /*
+    @dev The event 'Claimed' must be emitted when an airdrop is claimed
+    The address-type member 'account' takes receiver's address
+    The uint256-type member 'amount' takes token amount
+    The uint256-type member 'tokenId' takes Token ID
+    */
+    event Claimed(address account, uint256 amount, uint256 tokenId);
+    /*
+    @dev The event 'NFTMinted' must be emitted when an NFT is minted to an address
+    The address-type member 'account' takes receiver's address
+    The uint256-type member 'tokenId' takes Token ID
+    */
+    event NFTMinted(address account, uint256 tokenId);
     /*
     @dev The event 'NFTBurned' must be emitted when an NFT is burned from an address
     The address-type member 'account' takes receiver's address
     The uint256-type member 'tokenId' takes Token ID
     */
     event NFTBurned(address account, uint256 tokenId);
-
-
-    string private constant SIGNING_DOMAIN = "Voucher-Domain";
-    string private constant SIGNING_VERSION = "1";
+    /*
+    @dev The event 'RemovedFromAllowlist' must be emitted when an address is removed from allowlist
+    The address-type member 'account' takes receiver's address
+    The uint256-type member 'tokenId' takes Token ID
+    */
+    event RemovedFromAllowlist(address account, uint256 tokenId);
+    /*
+    @dev The event 'AddedToAllowlist' must be emitted when an address is added to allowlist
+    The address-type member 'account' takes receiver's address
+    The uint256-type member 'tokenId' takes Token ID
+    */
+    event AddedToAllowlist(address account, uint256 tokenId);
     /*
     The mapping 'MAX_COPIES' stores maximum number of copies
     The mapping 'minted' stores minted number of copies
@@ -55,181 +64,176 @@ contract NFT is ERC1155, ERC1155Burnable, EIP712, Ownable, ReentrancyGuard {
     mapping(uint256 => uint256) minted;
     uint256 creationTime;
     uint256 burnTime;
-
     /*
-    The struct 'NFTVoucher' is a struct for vouchers with all the info
-    The uint256-type member 'tokenId' stores Token ID
-    The uint256-type member 'minPrice' stores minimum price
-    The uint256-type member 'copies' stores copies
-    The address-type member 'artist' stores artist's address
-    The bytes-type member 'signature' stores signature
+    The mapping 'allowlist' stores allowlist/whitelist addresses
+    The mapping 'airdropped' stores number of tokens airdropped to an address
     */
-    struct NFTVoucher {
-        uint256 tokenId;
-        uint256 minPrice;
-        uint256 copies;
-        address artist;
-        /*The signature proves that the NFT creator authorized the creation 
-        of the specific NFT described in the voucher.
-        */
-        bytes signature;
+    mapping(uint256 => mapping(address => bool)) public allowlist;
+    mapping(uint256 => mapping(address => uint256)) public airdropped;
+    /*
+    The struct 'NFTAirdrop' is a struct for airdrops
+    The address-type member 'receiver' stores receiver's address
+    The uint256-type member 'amount' stores token amount
+    */
+    struct NFTAirdrop {
+        address receiver;
+        uint256 amount;
     }
-
-    /*
-    The mapping 'voucherUsed'  keeps a track of the used NFT Vouchers
-    The mapping 'airdropped'  keeps track of airdropped number of tokens
-    */
-    mapping(uint256 => bool) public voucherUsed;
-    mapping(address => uint256) public airdropped;
 
     /*
     The contract constructor takes 4 arguments while deploying
-    The uint256-type input 'longhorn_copies' takes number of copies for Longhorn
-    The uint256-type input 'memberpass_copies' takes number of copies for Member Pass
+    The uint256-type input 'contentPassCopies' takes number of copies for Content Pass
+    The uint256-type input 'socialPassCopies' takes number of copies for Social Pass
+    The address[]-type input '_whitelistForContentPass' takes whitelisted addresses for Content Pass
+    The address[]-type input '_whitelistForSocialPass' takes whitelisted addresses for Social Pass
     */
-    constructor(uint256 longhorn_copies, uint256 memberpass_copies)
-        ERC1155("LongHorn_MemberPass")
-        EIP712(SIGNING_DOMAIN, SIGNING_VERSION)
-    {
+    constructor(
+        uint256 contentPassCopies,
+        uint256 socialPassCopies,
+        address[] memory _whitelistForContentPass,
+        address[] memory _whitelistForSocialPass
+    ) ERC1155("CommunityBuilderPass") {
         creationTime = block.timestamp;
         //365(days)*24(hours)*60(minutes)*60(seconds) = 31536000 seconds
         burnTime = creationTime + 31536000;
-        MAX_COPIES[1] = longhorn_copies;
-        MAX_COPIES[2] = memberpass_copies;
+        MAX_COPIES[1] = contentPassCopies;
+        MAX_COPIES[2] = socialPassCopies;
+
+        for (uint256 i = 0; i < _whitelistForContentPass.length; i++) {
+            allowlist[1][_whitelistForContentPass[i]] = true;
+        }
+
+        for (uint256 i = 0; i < _whitelistForSocialPass.length; i++) {
+            allowlist[2][_whitelistForSocialPass[i]] = true;
+        }
     }
 
     /*
-    The function 'setURI' sets the base URI
-    The string-type input 'newuri' takes the URI
+    The function 'airdrop' can only be called by owner, 
+    and airdrops nfts to the account passed as param to the function
+    The uint256-type member 'tokenId' takes Token ID
+    The address-type member 'account' takes receiver's address
+    The uint256-type member 'amount' takes token amount
     */
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
-
-    /*
-    function 'redeem' takes a voucher as an argument and lets the user redeem the signed voucher
-    The NFTVoucher-type member 'voucher' takes voucher
-    The address-type member 'account' takes buyer's address
-    The uint256-type member 'nonce' takes nonce
-    */
-    function redeem(
-        NFTVoucher calldata voucher,
-        address buyer,
-        uint256 nonce,
-        uint256 copies
-    ) public payable nonReentrant {
-        /*
-        Check if the signature is valid and belongs to the account that's authorized to mint NFTs
-        */
-        address signer = _verify(voucher);
-        require(signer == voucher.artist, "Invalid signer");
+    function airdrop(
+        uint256 tokenId,
+        address account,
+        uint256 amount
+    ) external onlyOwner {
+        require(tokenId == 1 || tokenId == 2, "TokenId doesn't exists!");
 
         /*
-        Check to see if user has already used the signature
+        The require statement checks if number of airdropped nft exceeds Maximum Supply
         */
-        require(!voucherUsed[nonce], "This voucher has already been used.");
-
         require(
-            voucher.tokenId == 1 || voucher.tokenId == 2,
-            "Token doesn't exist"
-        );
-        require(
-            minted[voucher.tokenId] + copies <= MAX_COPIES[voucher.tokenId],
+            minted[tokenId] + 1 <= MAX_COPIES[tokenId],
             "Not enough supply"
         );
+
         require(
-            msg.value >= voucher.minPrice * copies * 1 wei,
-            "Not enough ethers sent"
+            allowlist[tokenId][account],
+            "Address not present in the allowlist"
         );
 
-        /*
-        transferring the funds to the owner
-        */
-        payable(owner()).transfer(msg.value);
-
-        /*
-        minting NFT
-        */
-        _mint(buyer, voucher.tokenId, copies, "");
-        minted[voucher.tokenId] += copies;
-        voucherUsed[nonce] = true;
-
-        /*
-        airdropping MemberPass NFT to the long horn NFT buyers
-        */
-        if (voucher.tokenId == 1) airdrop(buyer);
-
-        emit NFTPurchased(voucher.tokenId, nonce, copies, buyer, msg.value);
+        airdropped[tokenId][account] += amount;
+        emit Airdropped(account, amount, tokenId);
     }
 
     /*
-    The function 'airdrop' airdrops nfts to the account passed as param to the function
-    The address-type member 'account' takes receiver's address
+    The function 'airdropToMultipleAccount' can only be called by owner, 
+    and airdrops nfts to all the accounts passed as paramater to the function
+    The uint256-type member 'tokenId' takes Token ID
+    The NFTAirdrop[]-type input 'toAirdrop' takes array of 'NFTAirdrop' structs
     */
-    function airdrop(address account) internal {
-        /*
-        check if number of airdropped Member Pass Nft exceeds Maximum Supply
-        */
-        require(minted[2] + 1 <= MAX_COPIES[2], "Not enough supply");
+    function airdropToMultipleAccount(
+        uint256 tokenId,
+        NFTAirdrop[] memory toAirdrop
+    ) external onlyOwner {
+        require(tokenId == 1 || tokenId == 2, "TokenId doesn't exist");
 
-        airdropped[account]++;
-        emit Airdropped(account, 1);
+        for (uint256 i = 0; i < toAirdrop.length; i++) {
+            address receiver = toAirdrop[i].receiver;
+            uint256 amount = toAirdrop[i].amount;
+            /*
+            The require statement checks if number of airdropped nft exceeds Maximum Supply
+            */
+            require(
+                minted[tokenId] + amount <= MAX_COPIES[tokenId],
+                "Not enough supply"
+            );
+
+            if (allowlist[tokenId][receiver]) {
+                airdropped[tokenId][receiver] += amount;
+                emit Airdropped(receiver, amount, tokenId);
+            } else {
+                emit NotAirdropped(receiver, tokenId);
+            }
+        }
     }
 
     /*
-    The function 'noOfPassesAirdropped' returns the number of Member Pass NFTs airdropped to an account
+    The function 'noOfPassesAirdropped' returns the number of NFTs airdropped to an account
+    The uint256-type input 'tokenId' takes Token ID
     The address-type member 'account' takes user's address
-    Returns uint256-type value which is the number of Member Pass NFTs possessed by the specified account
+    Returns uint256-type value which is the number of NFTs of given tokenId possessed by the specified account
     */
 
-    function noOfPassesAirdropped(address account)
+    function noOfPassesAirdropped(uint256 tokenId, address account)
         public
         view
         returns (uint256)
     {
-        return airdropped[account];
+        return airdropped[tokenId][account];
+    }
+
+    /*
+    The function 'addToAllowlist' adds the list of addresses to the Allowlist
+    The address[]-type input '_recipients' takes addresses to add to the Allowlist
+    The uint256-type input 'tokenId' takes Token ID
+    */
+    function addToAllowlist(address[] memory _recipients, uint256 tokenId)
+        external
+        onlyOwner
+    {
+        require(tokenId == 1 || tokenId == 2, "TokenId doesn't exists!");
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            allowlist[tokenId][_recipients[i]] = true;
+            emit AddedToAllowlist(_recipients[i], tokenId);
+        }
+    }
+
+    /*
+    The function 'removeFromAllowlist' removes the list of addresses from the Allowlist
+    The address[]-type input '_recipients' takes addresses to remove from the Allowlist
+    The uint256-type input 'tokenId' takes Token ID
+    */
+    function removeFromAllowlist(address[] memory _recipients, uint256 tokenId)
+        external
+        onlyOwner
+    {
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            allowlist[tokenId][_recipients[i]] = false;
+            emit RemovedFromAllowlist(_recipients[i], tokenId);
+        }
     }
 
     /*
     The function 'claim' mints the nft to the address(msg.sender) who was aidropped the nft 
     The address-type input 'account' takes receiver's address
+    The uint256-type input 'tokenId' takes Token ID
     */
-    function claim(address account) external nonReentrant {
-        uint256 amount = airdropped[account];
+    function claim(address account, uint256 tokenId) external nonReentrant {
+        uint256 amount = airdropped[tokenId][account];
+
+        require(tokenId == 1 || tokenId == 2, "TokenId doesn't exists!");
         require(amount > 0, "You don't have any NFT!");
 
-        airdropped[account] = 0;
+        airdropped[tokenId][account] = 0;
 
-        _mint(account, 2, amount, "");
+        _mint(account, tokenId, amount, "");
+        minted[tokenId] += amount;
 
-        emit MemberPassClaimed(account, 2);
-    }
-
-    /*
-    The function 'burn' burns the nft only after 1 year
-    The address-type input 'account' takes  address
-    The uint256-type input 'id' takes Token ID
-    The uint256-type input 'value' takes value
-    */
-    function burn(
-        address account,
-        uint256 id,
-        uint256 value
-    ) public override {
-        require(
-            account == _msgSender() || isApprovedForAll(account, _msgSender()),
-            "ERC1155: caller is not token owner or approved"
-        );
-        /*
-        Require statement checks that the time has exceeded burn rate, i.e., 1 year
-        */
-        require(
-            block.timestamp > burnTime,
-            "You are not allowed to burn the nft before 1 year"
-        );
-
-        emit NFTBurned(account, id);
-        super.burn(account, id, value);
+        emit Claimed(account, amount, tokenId);
     }
 
     /*
@@ -245,41 +249,72 @@ contract NFT is ERC1155, ERC1155Burnable, EIP712, Ownable, ReentrancyGuard {
     }
 
     /*
-    The function '_verify' verifies signature against input and recovers address, 
-    or reverts transaction if signature is invalid
-    The NFTVoucher-type input 'voucher' takes voucher
+    The function 'setURI' sets the base URI
+    The string-type input 'newuri' takes the URI
     */
-    function _verify(NFTVoucher calldata voucher)
-        public
-        view
-        returns (address)
-    {
-        bytes32 digest = _hash(voucher);
-        return ECDSA.recover(digest, voucher.signature);
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
     /*
-    The function '_hash' returns the hash of the argument passed
-    The NFTVoucher-type input 'voucher' takes voucher
+    The function 'burn' burns the nft only after 1 year
+    The address-type input 'account' takes  address
+    The uint256-type input 'id' takes Token ID
+    The uint256-type input 'value' takes value
     */
-    function _hash(NFTVoucher calldata voucher)
-        internal
-        view
-        returns (bytes32)
-    {
-        return
-            _hashTypedDataV4(
-                keccak256(
-                    abi.encode(
-                        keccak256(
-                            "NFTVoucher(uint256 tokenId,uint256 minPrice,uint256 copies,address artist)"
-                        ),
-                        voucher.tokenId,
-                        voucher.minPrice,
-                        voucher.copies,
-                        voucher.artist
-                    )
-                )
-            );
+    function burn(
+        address account,
+        uint256 id,
+        uint256 value
+    ) public override {
+        require(id == 1 || id == 2, "TokenId doesn't exists!");
+
+        require(
+            account == _msgSender() || isApprovedForAll(account, _msgSender()),
+            "ERC1155: caller is not token owner or approved"
+        );
+        /*
+        Require statement checks that the time has exceeded burn rate, i.e., 1 year
+        */
+        require(
+            block.timestamp > burnTime,
+            "You are not allowed to burn the nft before 1 year"
+        );
+        super.burn(account, id, value);
+    }
+
+    /*
+    The function '_beforeTokenTransfer' ensures that NFT is non-transferable 
+    */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        require(
+            from == address(0) || to == address(0),
+            "You can't transfer this NFT"
+        );
+    }
+
+    /*
+    The function '_afterTokenTransfer' emits the event based on whether token is minted or burned.
+    */
+    function _afterTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        if (from == address(0)) {
+            emit NFTMinted(to, ids[0]);
+        } else if (to == address(0)) {
+            emit NFTBurned(to, ids[0]);
+        }
     }
 }
